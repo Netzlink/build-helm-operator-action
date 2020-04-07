0fef3122d8f75f9f8da38f31360ce0e0515bc90f
nk-designz <nka@netzlink.com>
2020-04-07 18:25:11 +0200

    curl issue

--- prerequisites.sh
+++ prerequisites.sh
@@ -5,10 +5,10 @@ apt-get install -y git curl
 echo "Adding helm3"
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 echo "Adding operator sdk"
-curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu -o operator-sdk
+curl -L https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu --output operator-sdk
 chmod +x operator-sdk
 mv operator-sdk /usr/bin
-echo "Installed operator sdk at verion $(operator-sdk --version)"
+echo "Installed operator sdk at version $(operator-sdk version)"
 echo "Removing curl"
 apt-get -y remove curl
 echo "Install ready"
