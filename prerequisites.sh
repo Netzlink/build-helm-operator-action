e5b71d55b60ed3f27d98a9db7431957198da8209
nk-designz <nka@netzlink.com>
2020-04-30 15:30:29 +0200

    moved from helm master to release

--- prerequisites.sh
+++ prerequisites.sh
@@ -3,7 +3,10 @@ echo "adding git, curl"
 apt-get update -y
 apt-get install -y git curl
 echo "Adding helm3"
-curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+# curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+curl https://get.helm.sh/helm-v3.2.0-linux-amd64.tar.gz > helm.tar.gz
+tar -zxvf helm.tar.gz
+mv linux-amd64/helm /usr/bin/helm
 echo "Adding operator sdk"
 curl -L https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu --output operator-sdk
 chmod +x operator-sdk
